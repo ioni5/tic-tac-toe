a42b50ffd9d9e276aeb6c7b65ccf16022555d42a
ioni5 <ioni5.goto@gmail.com>
2022-11-15 17:40:10 -0500

    refactor

--- src/main/java/io/github/ioni5/App.java
+++ src/main/java/io/github/ioni5/App.java
@@ -2,9 +2,9 @@ package io.github.ioni5;
 
 public class App {
 
-    public void play(int numPlayers) {
+    public void play() {
         do {
-            new TicTacToe(numPlayers).play();
+            new TicTacToe().play();
         } while (this.again());
     }
 
@@ -21,20 +21,7 @@ public class App {
     }
     
     public static void main( String[] args ) {
-        int numPlayers = -1;
-        try {
-            if (args.length != 1) {
-                throw new Exception("\nUso: Main<Número de jugadores>\n");
-            }
-            numPlayers = Integer.parseInt(args[0]);
-            if (!new Intervale(0, TicTacToe.MAX_NUM_PLAYERS).includes(numPlayers)) {
-                throw new Exception("\nNúmero de jugadores (0-" + TicTacToe.MAX_NUM_PLAYERS + ")\n");
-            }
-        } catch (Exception ex) {
-            new Console().write(ex.getMessage());
-            System.exit(0);
-        }
-        new App().play(numPlayers);
+        new App().play();
     }
 
 }
--- src/main/java/io/github/ioni5/TicTacToe.java
+++ src/main/java/io/github/ioni5/TicTacToe.java
@@ -2,43 +2,27 @@ package io.github.ioni5;
 
 public class TicTacToe {
 
-    static final int MAX_NUM_PLAYERS = 2;
-    
-    private Player[] players;
-
     private Turn turn;
 
     private Board board;
 
-    public TicTacToe(int numPlayers) {
-        players = new Player[MAX_NUM_PLAYERS];
-        for (int i = 0; i < numPlayers; i++) {
-            players[i] = new HumanPlayer(Token.values()[i]);
-        }
-        for (int i = numPlayers; i < MAX_NUM_PLAYERS; i++) {
-            players[i] = new MachinePlayer(Token.values()[i]);
-        }
-        turn = new Turn(players);
+    public TicTacToe() {
         board = new Board();
+        turn = new Turn(board);
     }
 
     public void play() {
         Console console = new Console();
         console.write("\nTic-Tac-Toe\n");
-        Player player;
         do {
             board.show();
-            player = turn.getPlayer();
-            player.play(board);
-            if (!this.gameover()) {
+            turn.play();
+            if (!board.hasWinner()) {
                 turn.next();
             }
-        } while (!this.gameover());
+        } while (!board.hasWinner());
         board.show();
-        new Console().write("\n¡Las '" + player.getToken() + "' ganan!\n");
+        turn.showWinner();
     }
 
-    private boolean gameover() {
-        return board.hasWinner();
-    }
 }
